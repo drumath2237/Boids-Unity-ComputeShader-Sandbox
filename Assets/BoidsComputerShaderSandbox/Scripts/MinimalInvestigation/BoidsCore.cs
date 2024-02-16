b11a060ce74bdd78befa620efad88c0009bcd1e3
drumath <2237k.drumath@gmail.com>
2024-02-17 03:08:11 +0900

    :sparkles: Boidsの処理を行うクラスを作成WIP

---
+++ Assets/BoidsComputerShaderSandbox/Scripts/MinimalInvestigation/BoidsCore.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace BoidsComputeShaderSandbox.MinimalInvestigation
+{
+    public struct BoidsOptions
+    {
+        public int Count { get; set; }
+        public float BoundingWidth { get; set; }
+        public float BoundingHeight { get; set; }
+        public float MaxVelocity { get; set; }
+        public float MaxAcceleration { get; set; }
+        public float InsightRange { get; set; }
+    }
+
+    public class BoidsCore
+    {
+        private readonly BoidsOptions _options;
+        private readonly Vector3[] _positions;
+        private readonly Vector3[] _velocities;
+        private readonly Vector3[] _accelerations;
+
+        public BoidsCore(BoidsOptions options)
+        {
+            _options = options;
+
+            _positions = new Vector3[options.Count];
+            _velocities = new Vector3[options.Count];
+            _accelerations = new Vector3[options.Count];
+        }
+
+        private static bool WithinRange(Vector3 self, Vector3 target, float range)
+            => (target - self).sqrMagnitude <= range * range;
+    }
+}
