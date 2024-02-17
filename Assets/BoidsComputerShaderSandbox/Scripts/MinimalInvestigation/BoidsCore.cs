421403a570c78e8156779690dac8f895372e5cea
drumath2237 <2237k.drumath@gmail.com>
2024-02-18 01:16:09 +0900

    :bug: boidsの全体処理のイメージを下書き
    ドラフトなのでバグを含んでいる

--- Assets/BoidsComputerShaderSandbox/Scripts/MinimalInvestigation/BoidsCore.cs
+++ Assets/BoidsComputerShaderSandbox/Scripts/MinimalInvestigation/BoidsCore.cs
@@ -28,6 +28,33 @@ namespace BoidsComputeShaderSandbox.MinimalInvestigation
             _accelerations = new Vector3[options.Count];
         }
 
+        public void Update(float deltaTime)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                // 内部でCalcIndividualAccを使ってacc配列を更新
+            }
+            
+            // 全部のaccが計算できたらaccをもとにvel,posを更新
+        }
+
+        /// <summary>
+        /// 個別のboidに対してシミュレーションの更新を行い
+        /// 次のフレームの加速度を出力する関数
+        /// </summary>
+        /// <param name="positions">boidsの位置配列</param>
+        /// <param name="velocities">boidsの速度配列</param>
+        /// <param name="accelerations">boidsの加速度配列</param>
+        /// <param name="index">計算対象のboidsのindex</param>
+        /// <param name="deltaTime">前回更新からの経過時間</param>
+        /// <returns></returns>
+        public static Vector3 CalcIndividualAcc(Vector3[] positions, Vector3[] velocities, Vector3[] accelerations,int index, float deltaTime)
+        {
+            // todo
+            return Vector3.Zero();
+        }
+        
+
         private static bool WithinRange(Vector3 self, Vector3 target, float range)
             => (target - self).sqrMagnitude <= range * range;
     }
