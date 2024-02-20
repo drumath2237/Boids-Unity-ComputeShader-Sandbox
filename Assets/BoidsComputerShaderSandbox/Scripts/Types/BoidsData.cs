984f39ec6f0cf97b4290911727aeb190483c8562
drumath <2237k.drumath@gmail.com>
2024-02-21 00:59:52 +0900

    :sparkles: BoidsData構造体の定義

---
+++ Assets/BoidsComputerShaderSandbox/Scripts/Types/BoidsData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+namespace BoidsComputeShaderSandbox.Types
+{
+    [VFXType(VFXTypeAttribute.Usage.GraphicsBuffer, "BoidsData")]
+    public struct BoidsData
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 Acceleration;
+
+        public BoidsData(Vector3 position, Vector3 velocity, Vector3 acceleration)
+        {
+            Position = position;
+            Velocity = velocity;
+            Acceleration = acceleration;
+        }
+
+        public void Deconstruct(
+            out Vector3 position,
+            out Vector3 velocity,
+            out Vector3 acceleration
+        )
+        {
+            position = Position;
+            velocity = Velocity;
+            acceleration = Acceleration;
+        }
+    }
+}
