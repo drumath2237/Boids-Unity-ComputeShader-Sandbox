ec968aaa34594d962dd350bba9f919aa1d293cb4
drumath <2237k.drumath@gmail.com>
2024-02-23 18:21:08 +0900

    :sparkles: property binderを仮で作ってみる

---
+++ Assets/BoidsComputerShaderSandbox/Scripts/VFX/BoidsBinder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+namespace BoidsComputeShaderSandbox.VFX
+{
+    [VFXBinder("Custom/Boids")]
+    public class BoidsBinder : VFXBinderBase
+    {
+        [VFXPropertyBinding(nameof(UnityEngine.GraphicsBuffer))]
+        public ExposedProperty boidsBufferProperty;
+
+        [VFXPropertyBinding(nameof(System.Int32))]
+        public ExposedProperty boidsCountProperty;
+
+        [SerializeField]
+        private int boidsCount;
+
+
+        public override bool IsValid(VisualEffect component)
+        {
+            var isCountValid = boidsCount > 0;
+            var isPropertyExist = component.HasGraphicsBuffer(boidsBufferProperty)
+                                  && component.HasInt(boidsCountProperty);
+
+            return isCountValid && isPropertyExist;
+        }
+
+        public override void UpdateBinding(VisualEffect component)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
